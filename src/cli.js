7028d811f4ce9bd0661f74ca8725a49d4cfd77b6
HE Shi-Jun <hax@heshijun.net>
2017-01-23 18:03:20 +0800

    unwrap

--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,7 @@ import {readFileSync} from 'fs'
 import pack from '../package'
 import command from '.'
 
-void function () {
-
-	const options = normalizeOptions(docopt(`
+const options = normalizeOptions(docopt(`
 Dependencies Cache
 
 Usage:
@@ -19,14 +17,12 @@ Options:
   -c <dir> --cacheDirectory=<dir>    Cache directory
 `))
 
-	if (options.version) {
-		console.log(pack.name, pack.version)
-	} else {
-		// console.log(options)
-		command(options)
-	}
-
-}()
+if (options.version) {
+	console.log(pack.name, pack.version)
+} else {
+	// console.log(options)
+	command(options)
+}
 
 function normalizeOptions(docopt) {
 	const opt = {}
